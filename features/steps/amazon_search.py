233a6be8efdfba2bcdd68a24a0b28d1e31303d63
inna <marmaxkat@gmail.com>
2021-01-27 15:33:29 -0800

    amazon search added

---
+++ features/steps/amazon_search.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+
+@given('Open Amazon page')
+def open_amazon(context):
+    context.driver.get('https://www.amazon.com/')
+
+@when('Input Watches into Amazon search field')
+def input_search(context)
+    search_field = context.driver.find_element(By.ID,'twotabsearchtextbox')
+    search_field.send_keys('Watches')
